c6bfb9216ae66c84403fa50326e6d8f36e385f75
shun-harutaro <shun_harutaro@protonmail.com>
2023-08-27 10:47:44 +0900

    チェックインAPI完成

--- functions/api/routes/users.js
+++ functions/api/routes/users.js
@@ -15,6 +15,8 @@ const {
 
 //const { defineString } = require("firebase-functions/params");
 //const clientId = defineString("CLIENT_ID");
+
+// Firestore
 const serviceAccount = require("../../credential.json");
 initializeApp({
   credential: cert(serviceAccount),
@@ -39,12 +41,31 @@ const register = async(profile) => {
   const userId = profile.sub;
   try {
     const res = await db.collection("users").doc(userId).set(data);
-    console.log(res);
+    return res;
   } catch(err) {
     console.log(err);
   }
 }
 
+const checkIn = async(userId, shopId) => {
+  const userShopHistoryRef = db.collection("users").doc(userId).collection("shop-history");
+  try {
+    const snapshot = await userShopHistoryRef.get();
+    if (snapshot.empty) {
+      console.log("there are not sub-collection shop-history")
+    }
+    const data = {
+      checkInAt: Timestamp.now(),
+      shopId: shopId
+    }
+    const res = await userShopHistoryRef.add(data);
+    return res;
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+// with LINE API
 const validateToken = async (idToken) => {
   try {
     const resValidateToken = await axios
@@ -68,7 +89,7 @@ const validateToken = async (idToken) => {
   }
 };
 
-
+// routing
 router.get("/:idToken", async (req, res) => {
   const idToken = req.params.idToken;
   try {
@@ -85,15 +106,30 @@ router.get("/:idToken", async (req, res) => {
   }
 });
 
-router.post("/register/:idToken", async(req, res) => {
+router.post("/:idToken", async(req, res) => {
   const idToken = req.params.idToken;
   try {
     const resValidateToken = await validateToken(idToken);
     const profile = resValidateToken.data;
     const result = await register(profile);
+    console.log(result)
     res.send("Register success");
   } catch(err) {
     res.status(400).send("Error to register")
+  }
+})
+
+router.put("/:idToken/shop/:shopId", async(req, res) => {
+  const idToken = req.params.idToken;
+  const shopId = req.params.shopId;
+  try {
+    const resValidateToken = await validateToken(idToken);
+    const userId = resValidateToken.data.sub;
+    await checkIn(userId, shopId)
+    res.send("Check in success");
+  } catch(err) {
+    console.log(err);
+    res.status(400).send("Error to check in")
   }
 })
 
--- hosting/routes/CheckIn.jsx
+++ hosting/routes/CheckIn.jsx
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from "react";
 import liff from "@line/liff";
+import axios from "axios";
 //import { Auth } from "../auth/Auth";
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 export const CheckIn = () => {
   const [qrCodeData, setQrCodeData] = useState('');
-  //const [result, setResult] = useState('');
+  const [result, setResult] = useState('');
+
+  const initLiff = async () => {
+    await liff.init({ liffId: process.env.REACT_APP_LIFF_ID });
+    if (liff.isInClient()) {
+      const idToken = liff.getIDToken();
+      await checkIn(idToken);
+    }
+  };
 
   useEffect(() => {
-    liff.init({ liffId: process.env.REACT_APP_LIFF_ID})
+    initLiff();
   }, []);
 
   const handleScan = () => {
@@ -16,10 +26,22 @@ export const CheckIn = () => {
     });
   };
 
+  const checkIn = async (idToken) => {
+    const url = new URL(`${BASE_URL}/api/users/${idToken}/shop/0`);
+    try {
+      const res = await axios.put(url);
+      setResult(JSON.stringify(res.data));
+      return true;
+    } catch (err) {
+      setResult(JSON.stringify(err.response.data));
+    }
+  };
+
   return (
       <div>
         <button onClick={handleScan}>Scan QR Code</button>
-        <p>{qrCodeData}</p>
+        <p>shopId: {qrCodeData}</p>
+        <p>result: {result}</p>
       </div>
   )
 };
